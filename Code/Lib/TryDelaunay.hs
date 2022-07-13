d0dee5074965adfaec6953fb2caf17a58a3fc61d
Andrew U Frank <frank@geoinfo.tuwien.ac.at>
2022-07-13 23:43:46 +0200

    produce data to convert to triple for triangulation

--- Code/Lib/TryDelaunay.hs
+++ Code/Lib/TryDelaunay.hs
@@ -31,7 +31,8 @@ module Lib.TryDelaunay
 import UniformBase
 -- import Algorithms.Geometry.DelaunayTriangulation.Naive
 
-import Data.Geometry
+import Data.Geometry 
+-- ( Point(Point2), Additive((^+^)) )
 
 
 -- module Demo.Delaunay where
@@ -56,8 +57,14 @@ import Data.Ext ( type (:+)(..) )
 
 import Linear.V2 ( V2(..) )
 import Data.List.NonEmpty ( NonEmpty, fromList )
-import Algorithms.Geometry.DelaunayTriangulation.Types (toPlanarSubdivision)
+import Algorithms.Geometry.DelaunayTriangulation.Types
+    ( Triangulation, toPlanarSubdivision, toPlaneGraph )
+-- import Data.PlaneGraph ( PlaneGraph )
+-- import Data.PlaneGraph ( PlaneGraph )
+import Data.Geometry.PlanarSubdivision
+    -- ( PlanarSubdivision, PlaneGraph, edgeSegment )
 
+import Data.Aeson.Encode.Pretty (encodePretty)
 
 
 p1 :: V2 Int 
@@ -72,16 +79,35 @@ q1 = Point2 4.5 3.3 :+ 'a'
 
 qs :: [Point 2 Float :+ Char]
 qs = [(Point2  0 0) :+ 'a' , Point2  1.5 1.5 :+ 'b' , Point2  0 2  :+ 'c', Point2  2 0  :+ 'd']
+qs2 :: [Point 2 Float]
+qs2 = [(Point2  0 0) , Point2  1.5 1.5  , Point2  0 2  , Point2  2 0  ]
+qsColl :: [Point 2 Integer :+ Char]
 qsColl = [(Point2  0 0) :+ 'a' , Point2  1 1 :+ 'b' , Point2  0 2  :+ 'c', Point2  2 0  :+ 'd']-- fails with b 1 1 ??
 
--- qsne = fmap (:+()) $ fromList qs
-qsne =  fromList qs
+qsne1 = fmap (:+()) $ fromList qs
+qsne2 :: NonEmpty (Point 2 Float :+ Char)
+qsne2 =  fromList qs
+qsne3 :: NonEmpty (Point 2 Float :+ Int)
+qsne3 = fromList . zipWith (:+) qs2 $ [1..]
 
+t1 :: Triangulation Char Float
+t1 = delaunayTriangulation qsne2 
 
-t1 = delaunayTriangulation qsne 
+g1 :: PlanarSubdivision s Char () () Float
 g1 = toPlanarSubdivision t1
+pg1 :: forall k (s :: k). PlaneGraph s Char () () Float
+pg1 = toPlaneGraph t1  --
+
+pg2 = toPlaneGraph . delaunayTriangulation $ qsne3
+-- pg1json = toJSON pg1 
+pg1pretty = encodePretty pg1 
+pg2pretty = encodePretty  pg2
+-- planeg1 = writePlaneGraph pg1pretty
 -- NonEmpty.fromList [4, 5]
 -- p2 :: Point 2 Int
+
+-- sg2 = edgeSegments pg2
+v2s = vertices g1
 mainDel1:: IO ()
 mainDel1 = do 
     putIOwords ["mainDel"]
@@ -91,5 +117,8 @@ mainDel1 = do
     -- putIOwords ["qsFails", showT . delaunayTriangulation . fromList $ qsColl]
     putIOwords ["t1", showT t1]
     putIOwords ["g1", showT g1]
-
+    putIOwords ["the json format of the planeGraph", showT pg1]
+    putIOwords ["the json format of the planeGraph",   bl2t pg1pretty]
+    putIOwords ["the json format of the planeGraph pg2",   bl2t pg2pretty]
+    -- putIOwords ["sg2", showT sg2]
     return ()
